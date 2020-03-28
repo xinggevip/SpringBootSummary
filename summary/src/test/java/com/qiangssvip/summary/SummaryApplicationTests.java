526f839c62bd240ef96265a495ba2e2bb2faa3b0
xinggevip <1511844263@qq.com>
2020-03-28 17:09:24 +0800

    项目初始化

---
+++ summary/src/test/java/com/qiangssvip/summary/SummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiangssvip.summary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
